97a7fdb076f366f8cf1501fec8c5c06a51915150
Ezequiel Ludueña <ezeluduena123@gmail.com>
2026-04-17 16:43:16 -0300

    refact: submodules and copy_addons.sh script

--- copy_addons.sh
+++ copy_addons.sh
@@ -4,8 +4,9 @@ RUTA_ORIGEN="./submodules/"
 RUTA_DESTINO="./custom-addons/"
 EXCLUIR="./exclude.txt"
 
-# Iterar sobre cada subdirectorio en ./submodules
-for dir in "$RUTA_ORIGEN"*/; do
-  echo "Copiando desde $dir a $RUTA_DESTINO"
+# Buscar recursivamente directorios que sean módulos de Odoo (contienen __manifest__.py)
+find "$RUTA_ORIGEN" -type f -name "__manifest__.py" -print0 | while IFS= read -r -d '' manifest; do
+  dir=$(dirname "$manifest")
+  echo "Copiando módulo desde $dir a $RUTA_DESTINO"
   rsync -av --exclude-from="$EXCLUIR" "$dir" "$RUTA_DESTINO"
 done
